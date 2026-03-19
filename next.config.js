51b88d53c9e3ef5f57f962566073878c4a135f9a
shravan-qyndex <admin@qyndex.com>
2026-03-20 00:02:26 +1100

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+};
+
+module.exports = nextConfig;
